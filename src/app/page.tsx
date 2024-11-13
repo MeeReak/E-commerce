758a8f799f16bae2adbad4b180c2b9ba4ac4e7da
MeeReak <meereak168@gmail.com>
2024-11-13 15:56:27 +0700

    new component: update banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import ProductCard from "./components/atoms/ProductCardX";
-import { productCard as Product } from "@/utils/mockup";
-
 export default function Home() {
   return (
     <>
-      <div className="h-screen flex items-center gap-x-5 justify-center">
-        {Product.map((product) => (
-          <ProductCard
-            key={product.id}
-            imageUrl={product.src}
-            price={product.price}
-            rating={product.rating}
-            title={product.name}
-          />
-        ))}
-      </div>
+      <div className="h-screen flex items-center justify-center"></div>
     </>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,60 +1,60 @@
 import React from "react";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 interface IBannerProps {
   subTitle?: string;
   title?: string;
   children?: React.ReactNode;
-  backgroundImage?: string; // Optional prop to pass the background image URL
+  backgroundImage?: string;
 }
 
-export const Banner = ({
+export const Banner: React.FC<IBannerProps> = ({
+  subTitle = "Best Deals",
+  title = "Sale of the Month",
   children,
-  subTitle,
-  title,
-  backgroundImage,
-}: IBannerProps) => {
+  backgroundImage = "/images/banner-vegetable.png",
+}) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${
-          backgroundImage || "/images/banner-vegetable.png"
-        })`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        padding: "2rem", // Adds padding to avoid overlapping
-      }}
-      className="text-white w-[424px] h-[536px] flex flex-col items-center "
-    >
-      <div className="flex items-center flex-col gap-y-4 pb-7">
-        <p className=" text-[14px] font-medium leading-none tracking-[0.42px] uppercase">
+    <div className="relative">
+      <Image
+        src={backgroundImage}
+        className="w-[424px] h-[536px]"
+        width={424}
+        height={536} // Adjusted height to match the aspect ratio of the image
+        alt="banner"
+      />
+      <div className="absolute top-[35px] w-full flex flex-col items-center justify-center">
+        <p className="text-white text-center text-sm font-medium mb-4 leading-[14px] tracking-[0.42px] uppercase">
           {subTitle}
         </p>
-        <p className="text-[40px] font-semibold leading-[48px]">{title}</p>
+        <p className="text-white text-center text-4xl mb-2 font-semibold leading-[48px]">
+          {title}
+        </p>
         <div>{children}</div>
+        <Button
+          variant="outline"
+          className="rounded-full mt-6 border-none"
+          rightIcon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0L13.5 19.5M21 12H3"
+              />
+            </svg>
+          }
+        >
+          Shop Now
+        </Button>
       </div>
-      <Button
-        variant={"outline"}
-        className="rounded-full"
-        rightIcon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-            />
-          </svg>
-        }
-      >
-        Shop Now
-      </Button>
     </div>
   );
 };
