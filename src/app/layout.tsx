3ec5fdfa271c631047a8c948304e4cc01b2c4ead
oengzhileang <zhileangCC990@gmail.com>
2024-11-27 21:40:35 -0500

    shopping-cart-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Navigation } from "@/components/Navigation";
-import Footer from "./components/templates/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,7 +32,6 @@ export default function RootLayout({
       >
         <Navigation />
         {children}
-        <Footer />
       </body>
     </html>
   );
