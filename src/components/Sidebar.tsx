9004db81f79ba3a28e86d9837f69f8eae5f909c9
MeeReak <meereak168@gmail.com>
2024-11-30 14:38:52 +0700

    blog list page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Input } from "./ui/input";
-import { SearchIcon } from "lucide-react";
+import { CalendarIcon, SearchIcon } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Sidebar = () => {
   const Category = [
@@ -25,18 +26,25 @@ export const Sidebar = () => {
     "Launch",
   ];
 
-  // const Images = [
-  //   "images/blog-vegetable.jpg",
-  //   "images/blog-brocoli.png",
-  //   "images/salad.avif",
-  //   "images/chilli.jpg",
-  //   "images/blog-carrot.jpg",
-  //   "images/blog-pea.webp",
-  //   "images/blog-orange.png",
+  const ImagesBlog = [
+    "/images/blog-vegetable.avif",
+    "/images/blog-brocoli.png",
+    "/images/blog-salad.avif",
+    "/images/blog-chilli.jpg",
+    "/images/blog-carrot.jpg",
+    "/images/blog-pea.webp",
+    "/images/blog-orange.png",
+    "/images/blog-cucumber.jpg",
+  ];
+
+  // const Recent = [
+  //   "/images/blog-mango.png",
+  //   "/images/blog-orange.png",
+  //   "/images/blog-black-berry.png",
   // ];
 
   return (
-    <div className=" w-[30%]">
+    <div className=" w-[33%]">
       <div className="pb-7 border-b border-gray-200">
         <Input
           className="w-[424px] py-5 rounded-md "
@@ -66,7 +74,7 @@ export const Sidebar = () => {
           ))}
         </div>
       </div>
-      <div>
+      <div className=" pb-7 border-b border-gray-200">
         <p className="text-gray-900 pt-5 text-lg font-medium leading-7 ">
           Popular Tag
         </p>
@@ -82,7 +90,41 @@ export const Sidebar = () => {
           ))}
         </div>
       </div>
-      <div></div>
+      <div className=" flex gap-2 pt-7 flex-wrap pb-7 border-b border-gray-200 ">
+        {ImagesBlog.map((item, index) => (
+          <Image
+            key={index}
+            className=" object-cover w-[100px] h-[100px] rounded-sm"
+            src={item}
+            alt={`Blog image ${index}`}
+            width={100}
+            height={100}
+          />
+        ))}
+      </div>
+      <div className=" pb-7 border-b pt-7 border-gray-200 ">
+        <p className="pb-4">Recently Added</p>
+        <div className="flex items-center gap-x-3">
+          <Image
+            alt="blog"
+            className=" w-[100px] h-[70px] rounded-sm"
+            src={"/images/blog-mango.png"}
+            width={100}
+            height={100}
+          />
+          <div>
+            <p className=" text-gray-900 text-base font-medium leading-6">
+              Curabitur porttitor orci eget nequ accumsan.
+            </p>
+            <div className=" flex gap-x-2">
+              <CalendarIcon className=" size-5 text-[#00B307]" />
+              <p className="text-gray-600 text-sm font-normal leading-6">
+                Apr 25, 2021
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
