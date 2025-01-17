91d67551d983f7b8094e4f466f374fcfca8a3b0c
MeeReak <meereak168@gmail.com>
2025-01-17 16:45:41 +0700

    fix: update package versions; enhance Select component with controlled value and improved props and redeploy

--- dashboard/src/components/Select.tsx
+++ dashboard/src/components/Select.tsx
@@ -14,23 +14,28 @@ export function SelectDemo({
   item,
   className = "",
   placeHolder,
+  selectedValue,
+  onSelectChange,
 }: {
   item: string[];
   className?: string;
   placeHolder?: string;
-  query?: string;
-  onSelectChange?: (query: string, value: string) => void;
+  selectedValue?: string; // Controlled value
+  onSelectChange: (value: string) => void; // Callback to notify parent
 }) {
   return (
-    <Select>
+    <Select
+      value={selectedValue} // Bind to the controlled value
+      onValueChange={onSelectChange} // Notify parent when value changes
+    >
       <SelectTrigger className={`${className}`}>
-        <SelectValue placeholder={placeHolder || item[0]} />
+        <SelectValue placeholder={placeHolder || "Select an option"} />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           {item.map((option, index) => (
             <SelectItem
-              className=" cursor-pointer"
+              className="cursor-pointer"
               key={index}
               value={option.toLowerCase()}
             >
